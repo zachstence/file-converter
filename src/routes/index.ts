14f81efb828d0ede51d4479e54d153659eaba15e
Zachary Stence <zachstence@gmail.com>
2022-05-16 22:39:15 -0500

    feat: proof of concept file conversion

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,20 +2,35 @@ import type { RequestHandler } from "@sveltejs/kit";
 import type { RequestBody } from "../lib/types/request-body";
 import { writeFileSync } from "fs";
 import { v4 as uuid } from "uuid";
+import im from "imagemagick";
 
+const FROM = "png";
+const TO = "jpg";
 
 export const post: RequestHandler = async ({ request }) => {
     const { files } = await request.json() as RequestBody;
     
-    const names = Array.from({ length: files.length }).map(_ => uuid());
+    const promises: Promise<void>[] = []
 
-    for (const [i, file] of files.entries()) {
+    for (const file of files) {
         const data = file.replace(/^data:image\/\w+;base64,/, "");
         const buf = Buffer.from(data, "base64");
-        writeFileSync(`${names[i]}.png`, buf);
 
-        
+        const name = uuid();
+        writeFileSync(`${name}.${FROM}`, buf); // TODO async
+
+        const p = new Promise<void>((resolve, reject) => {
+            im.convert([`${name}.${FROM}`, `${name}.${TO}`], (err, result) => {
+                if (err) reject();
+                else resolve();
+            });
+        });
+        promises.push(p);
     }
+
+    await Promise.all(promises);
+
+
 
     return {}
 };
