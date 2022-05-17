39c2244cfcb60c18113beacf4271479db7fda86f
Zachary Stence <zachstence@gmail.com>
2022-05-16 21:56:36 -0500

    feat: file upload

---
+++ src/lib/components/ImageFile.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    export let file: File;
+
+    let src: string | undefined;
+
+    const reader = new FileReader();
+
+    reader.onload = e => {
+        src = e.target?.result?.toString()
+    }
+    reader.readAsDataURL(file);
+</script>
+
+
+{#if src}
+    <img src={src} alt={file.name} height={200} />
+{/if}
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,42 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import ImageFile from "$lib/components/ImageFile.svelte";
+
+    let files: FileList | undefined;
+</script>
+
+
+<main>
+    <label>
+        Upload files
+        <input type="file" multiple bind:files={files} />
+    </label>
+
+    <div class="files">
+        {#if files}
+            {#each files as file}
+                <ImageFile {file} />
+            {/each}
+        {/if}
+        
+    </div>
+</main>
+
+
+<style>
+    :global(body) {
+        width: 100vw;
+        height: 100vh;
+        margin: 0;
+        background-color: #35363A;
+        color: white;
+    }
+    main {
+        height: 100%;
+        width: 100%;
+
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+    }
+</style>
