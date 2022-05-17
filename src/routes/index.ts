5165825f99d910e3912df7d36a6f089d06e0f311
Zachary Stence <zachstence@gmail.com>
2022-05-16 22:28:26 -0500

    feat: poc writing file

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,16 @@
 import type { RequestHandler } from "@sveltejs/kit";
-import type { RequestBody } from "$lib/types/request-body";
+import type { RequestBody } from "../lib/types/request-body";
+import { writeFileSync } from "fs";
+
 
 export const post: RequestHandler = async ({ request }) => {
-    const body = await request.json();
+    const body = await request.json() as RequestBody;
 
-    console.log(Object.keys(body));
+    for (const [i, file] of body.files.entries()) {
+        const data = file.replace(/^data:image\/\w+;base64,/, "");
+        const buf = Buffer.from(data, "base64");
+        writeFileSync(`image${i}.png`, buf);
+    }
 
     return {}
 };
