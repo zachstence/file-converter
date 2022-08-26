5b8ecd2a61516a4067461efc661aae2bfbde83c7
Zachary Stence <zachstence@gmail.com>
2022-08-25 23:09:32 -0500

    feat: proof of concept minio upload

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,17 +2,15 @@
     import type { UploadUrlResponse } from "./upload-url";
 
     let uploadUrl: string | undefined
-    let file: File | undefined
+    let files: FileList | undefined
 
-    $: console.log({file, uploadUrl})
+    $: console.log({files, uploadUrl})
 
     const onGetUploadUrl = async (): Promise<void> => {
-        console.log(file)
-        if (!file) return
-        uploadUrl = await getUploadUrl(file)
+        uploadUrl = await getUploadUrl()
     }
 
-    const getUploadUrl = async (file: File): Promise<string> => {
+    const getUploadUrl = async (): Promise<string> => {
         const response = await fetch(`/upload-url`)
         if (response.ok) {
             const json = await response.json() as UploadUrlResponse
@@ -20,14 +18,41 @@
         }
         throw response
     }
+
+    const upload = async (): Promise<void> => {
+        const fileReader = new FileReader()
+        fileReader.onloadend = async () => {
+            console.log('file loaded')
+            if (!uploadUrl || !files || !files.length) return
+            const file = files[0]
+            console.log(file)
+
+            console.log('uploading')
+            const response = await fetch(uploadUrl, {
+                method: 'PUT',
+                body: fileReader.result,
+                headers: {
+                    'Content-Type': file.type,
+                },
+            })
+            console.log('done uploading')
+        }
+
+        if (!files || !files.length) return
+        fileReader.readAsArrayBuffer(files[0])
+    }
 </script>
 
-<input type="file" bind:value={file} />
+<input type="file" bind:files />
 
-<pre>
-    {JSON.stringify(file, null, 2)}
-</pre>
+{#if files && files[0]}
+    <pre>{files[0].name}</pre>
+    <pre>{files[0].type}</pre>
+    <pre>{JSON.stringify(files[0], null, 2)}</pre>
+{/if}
 
 <button on:click={onGetUploadUrl}>Get Upload URL</button>
 
 <pre>{uploadUrl}</pre>
+
+<button on:click={upload}>Upload</button>
